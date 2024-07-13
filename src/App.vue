5856f3089d71aba6d4ba49473bc692d2ebd00641
Samuel Pak <89568922+skpak24@users.noreply.github.com>
2024-07-13 15:58:09 -0400

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script>
 
 import axios from 'axios';
+import FAQSection from './components/FAQSection.vue';
 
 export default {
+    components: {
+        FAQSection
+    },
     data() {
         return {
             newJob: {
@@ -19,31 +23,51 @@ export default {
     },
     methods: {
         fetchJobs() {
-            // get all jobs
+        // get all jobs
         axios.get('http://127.0.0.1:5000/api/jobs')
             .then(response => {
                 this.jobs = response.data;
+                expanded: false
             })
             .catch(error => {
                 console.error('Error fetching jobs:', error);
                 alert('Failed to fetch jobs. Please refresh the page.');
             });
         },
+        toggleJob(index) {
+            this.jobs[index].expanded = !this.jobs[index].expanded;
+        },
         addStep() {
             this.newJob.steps.push({
-                description: '',
-                hazards: '',
-                mitigation: ''
+                stepDesc: '',
+                haz: [{ 
+                    hazDesc: '', 
+                    mit: ['']
+                }]
             });
+        },
+        addHaz(stepIndex) {
+            this.newJob.steps[stepIndex].haz.push({
+                hazDesc: '',
+                mit: ['']
+            });
+        },
+        addMit(stepIndex, hazIndex) {
+            this.newJob.steps[stepIndex].haz[hazIndex].mit.push('');
         },
         removeStep(index) {
             this.newJob.steps.splice(index, 1);
+        },
+        removeHaz(stepIndex, hazIndex) {
+            this.newJob.steps[stepIndex].haz.splice(hazIndex, 1);
+        },
+        removeMit(stepIndex, hazIndex, mitIndex) {
+            this.newJob.steps[stepIndex].haz[hazIndex].mit.splice(mitIndex, 1);
         },
         submitJob() {
             if (this.newJob.steps.length > 0) {
                 axios.post('http://127.0.0.1:5000/api/jobs', this.newJob)
                 .then(response => {
-                    console.log('Job added successfully:', response.data);
                     this.fetchJobs(); // refetch jobs
                     this.clearForm(); // empty form
                     alert('Job added successfully');
@@ -57,11 +81,27 @@ export default {
             }
         },
         editJob(index) {
-            // fill form with preexisting JHA
-            this.newJob.name = this.jobs[index].name;
-            this.newJob.jobTitle = this.jobs[index].jobTitle;
-            this.newJob.steps = this.jobs[index].steps.map(step => ({ ...step }));
-            this.editingIndex = index;
+            if (this.editingIndex == index) {
+                this.clearForm(); // undo autofill
+            } else {
+                // autofill form with preexisting JHA
+                const selectedJob = this.jobs[index];
+
+                this.newJob.name = selectedJob.name;
+                this.newJob.jobTitle = selectedJob.jobTitle;
+                this.newJob.steps = selectedJob.steps.map(step => {
+                    return {
+                        stepDesc: step.stepDesc,
+                        haz: step.haz.map(haz => {
+                            return {
+                                hazDesc: haz.hazDesc,
+                                mit: haz.mit.map(mitigation => mitigation.mitDesc)
+                            };
+                        })
+                    };
+                });
+                this.editingIndex = index;
+            }
         },
         updateJob() {
             if (this.editingIndex > -1) {
@@ -124,14 +164,14 @@ export default {
                     <div class="row">
                         <div class="form-group">
                             <div class="row">
-                            <div class="col-6">
-                            <label>Full Name</label>
-                            <input type="text" v-model="newJob.name" class="form-control" required>
-                            </div>
-                            <div class="col-6">
-                            <label>Job Title</label>
-                            <input type="text" v-model="newJob.jobTitle" class="form-control" required>
-                            </div>
+                                <div class="col-6">
+                                    <label>Full Name</label>
+                                    <input name="name" type="text" v-model="newJob.name" class="form-control" required>
+                                </div>
+                                <div class="col-6">
+                                    <label>Job Title</label>
+                                    <input name="jobTitle" type="text" v-model="newJob.jobTitle" class="form-control" required>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -139,27 +179,57 @@ export default {
                     <div style="height: 20px;"></div>
             
                     <!-- dynamic fields -->
-                    <div v-for="(step, index) in newJob.steps" :key="index" class="card mb-3">
+                    <div v-for="(step, stepIndex) in newJob.steps" :key="stepIndex" class="card mb-3">
                         <div class="card-header">
-                            <h5 class="mt-1 text-center">Step {{ index + 1 }} Attributes</h5>
+                            <h5 class="mt-1 text-center">Step {{ stepIndex + 1 }} Attributes</h5>
                         </div>
                         <div class="card-body">
                             <div class="form-group">
                                 <label>Description</label>
-                                <textarea v-model="step.description" class="form-control" rows="3" required></textarea>
-                            </div>
-                            <div class="form-group">
-                                <label>Hazards</label>
-                                <textarea v-model="step.hazards" class="form-control" rows="3" required></textarea>
-                            </div>
-                            <div class="form-group">
-                                <label>Mitigation</label>
-                                <textarea v-model="step.mitigation" class="form-control" rows="3" required></textarea>
+                                <textarea v-model="step.stepDesc" class="form-control" rows="3" required></textarea>
                             </div>
 
-                            <br>
+                            <div style="height: 10px;"></div>
+                            <hr>   
+                            <div style="height: 10px;"></div>
 
-                            <button type="button" class="btn btn-danger" @click="removeStep(index)">Remove Step</button>
+                            <div v-for="(haz, hazIndex) in step.haz" :key="hazIndex" class="card mb-2">
+                                <div class="card-header">
+                                    <h6 class="mt-3 ms-2">Hazard {{ hazIndex + 1 }}
+                                        <span style="float:right;">
+                                            <button type="button" class="btn btn-danger btn-sm float-end mb-2" @click="removeHaz(stepIndex, hazIndex)">Remove Hazard</button>
+                                        </span>
+                                    </h6>
+                                </div>
+                                <div class="card-body">
+                                    <div class="form-group">
+                                        <label>Description</label>
+                                        <textarea v-model="haz.hazDesc" class="form-control" rows="2" required></textarea>
+                                    </div>
+                                    
+                                    <div style="height: 30px;"></div>
+
+                                    <div v-for="(mit, mitIndex) in haz.mit" :key="mitIndex" class="form-group">
+                                        <label>Mitigation {{ mitIndex + 1 }}</label>
+                                        <div class="row">
+                                            <div class="col-10">
+                                                <textarea v-model="haz.mit[mitIndex]" class="form-control" rows="2" required></textarea>
+                                            </div>
+                                            <div class="col-2">
+                                                <button type="button" class="btn-close btn-sm float-end mt-3 me-2" @click="removeMit(stepIndex, hazIndex, mitIndex)"></button>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div style="height: 10px;"></div>
+                                    <div class="text-center">
+                                        <button type="button" class="btn btn-secondary mt-2" @click="addMit(stepIndex, hazIndex)">Add Mitigation</button>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button type="button" class="btn btn-secondary me-2 mt-2" @click="addHaz(stepIndex)">Add Hazard</button>
+                            <button type="button" class="btn btn-danger mt-2" @click="removeStep(stepIndex)">Remove Step</button>
                         </div>
                     </div>
 
@@ -176,96 +246,9 @@ export default {
                 <hr>   
                 <div style="height: 10px;"></div>
 
-                <!-- FAQ -->
-                <div class="container">
-                    <h2 class="mb-4 text-center">Frequently Asked Questions</h2>
-                    <div class="accordion" id="accordionFAQ">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingOne">
-                            <button class="accordion-button collapsed fw-semibold fs-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                                What is a Job Hazard Analysis (JHA)?
-                            </button>
-                            </h2>
-                            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionFAQ">
-                                <div class="accordion-body fs-6">
-                                    A JHA is a method for identifying and evaluating hazards associated with tasks (steps) with a specific job or activity and eliminating or mitigating them prior to conducting work.
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingTwo">
-                            <button class="accordion-button collapsed fw-semibold fs-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                What are the steps in completing a JHA?
-                            </button>
-                            </h2>
-                            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionFAQ">
-                                <div class="accordion-body fs-6">
-                                    <p>1. Select the job/activity to be analyzed.</p>
-                                    <p>2. Break the job/activity down into a series of tasks.</p>
-                                    <p>3. Identify potential hazards and consequences in each task.</p>
-                                    <p>4. Determine preventive measures to overcome these hazards.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingThree">
-                            <button class="accordion-button collapsed fw-semibold fs-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                Who should conduct a JHA? 
-                            </button>
-                            </h2>
-                            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionFAQ">
-                                <div class="accordion-body fs-6">
-                                    Individuals who perform the tasks that are being evaluated should always conduct a JHA.
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingFour">
-                            <button class="accordion-button collapsed fw-semibold fs-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                                Why conduct a JHA?  
-                            </button>
-                            </h2>
-                            <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionFAQ">
-                                <div class="accordion-body fs-6">
-                                    A JHA can prevent work-related injuries or illnesses by eliminating or controlling identified hazards. It is a means to ensure that workers have the training, equipment, and supplies to do their jobs safely.
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingFive">
-                            <button class="accordion-button collapsed fw-semibold fs-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                                What do I do when the JHA is completed?
-                            </button>
-                            </h2>
-                            <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionFAQ">
-                                <div class="accordion-body fs-6">
-                                    <p>1. Ensure equipment and supplies are acquired to perform the job/activity according to the JHA.</p>
-                                    <p>2. Train workers in the procedures, controls, and PPE according to the JHA.</p>
-                                    <p>3. Review the JHA periodically and update it when the job/activity or equipment changes. Revise JHA as needed and retrain employees on the revised JHA.</p>
-                                    <p>4. Consult your administrative unit to review JHAs as needed.</p> 
-                                </div>  
-                            </div>
-                        </div>
-
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingSix">
-                            <button class="accordion-button collapsed fw-semibold fs-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-                                Other Information
-                            </button>
-                            </h2>
-                            <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionFAQ">
-                                <div class="accordion-body fs-6">
-                                    <p>The information in this FAQ section was obtained from the University of Washington's Environmental Health & Safety administrative unit (EH&S). More information regarding JHA's are available at the link below:</p>
-                                    <p><a class="link-opacity-100" href="https://www.ehs.washington.edu/workplace/job-hazard-analysis">Job Hazard Analysis | EHS</a></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <!-- FAQ -->
+            <FAQSection />
+            
             </div>
 
             <div class="col-6">
@@ -273,24 +256,36 @@ export default {
                 <div class="mx-2 text-center">
                     <h2 class="my-3 mb-4">Submitted JHA's</h2>
                     <div v-for="(job, index) in jobs" :key="index" class="card mb-3">
+                        
                         <div class="card-header">
-                            <h4 class="mt-1">{{ job.jobTitle }}</h4>
-                            <h6>Submitted by {{ job.name }}</h6>
-                            <button class="btn btn-sm btn-primary me-1" @click="editJob(index)">Edit</button>
-                            <button class="btn btn-sm btn-danger" @click="deleteJob(job.id)">Delete</button>
+                            <h3 class="my-3"><strong>{{ job.jobTitle }}</strong></h3>
+                            <h6 class="mt-4" style="text-align:left;">Submitted by {{ job.name }}
+                                <span style="float:right;">
+                                    <button class="btn btn-sm btn-warning me-2 mb-1" @click="editJob(index)">Edit</button>
+                                    <button class="btn btn-sm btn-danger mb-1" @click="deleteJob(job.id)">Delete</button>
+                                </span>
+                            </h6>
                         </div>
+
                         <ul class="list-group list-group-flush">
                             <li class="list-group-item" v-for="(step, sIndex) in job.steps" :key="sIndex">
-                                <strong>Step {{ sIndex + 1 }}:</strong><br>
-                                <strong>Description - </strong> {{ step.description }}<br>
-                                <strong>Hazards - </strong> {{ step.hazards }}<br>
-                                <strong>Mitigation - </strong> {{ step.mitigation }}
+                                <h5 class="mt-1"><strong>Step {{ sIndex + 1 }}: </strong>{{ step.stepDesc }}</h5>
+                                <div v-for="(hazard, hIndex) in step.haz" :key="hIndex">
+                                    <hr>
+                                    <h6 class="mb-3"><strong>Hazard {{ hIndex + 1 }}: </strong>{{ hazard.hazDesc }}</h6>
+                                    <p><strong>Mitigations: </strong></p>
+                                    <div class="container">
+                                        <div v-for="(mitigation, mIndex) in hazard.mit" :key="mIndex">
+                                            <p style="line-height:1.0;">- {{ mitigation.mitDesc }}</p>
+                                        </div>
+                                    </div>
+                                </div>
                             </li>
                         </ul>
+
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
 
