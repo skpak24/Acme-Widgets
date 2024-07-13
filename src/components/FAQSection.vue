a47c8d35eaf8ece25ea52b6d815b3583f6f43e3a
Samuel Pak <89568922+skpak24@users.noreply.github.com>
2024-07-13 16:01:08 -0400

    Add files via upload

---
+++ src/components/FAQSection.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="container">
+        <h2 class="mb-4 text-center">Frequently Asked Questions</h2>
+        <div class="accordion" id="accordionFAQ">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                <button class="accordion-button collapsed fw-semibold fs-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                    What is a Job Hazard Analysis (JHA)?
+                </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionFAQ">
+                    <div class="accordion-body fs-6">
+                        A JHA is a method for identifying and evaluating hazards associated with tasks (steps) with a specific job or activity and eliminating or mitigating them prior to conducting work.
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                <button class="accordion-button collapsed fw-semibold fs-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    What are the steps in completing a JHA?
+                </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionFAQ">
+                    <div class="accordion-body fs-6">
+                        <p>1. Select the job/activity to be analyzed.</p>
+                        <p>2. Break the job/activity down into a series of tasks.</p>
+                        <p>3. Identify potential hazards and consequences in each task.</p>
+                        <p>4. Determine preventive measures to overcome these hazards.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                <button class="accordion-button collapsed fw-semibold fs-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Who should conduct a JHA? 
+                </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionFAQ">
+                    <div class="accordion-body fs-6">
+                        Individuals who perform the tasks that are being evaluated should always conduct a JHA.
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingFour">
+                <button class="accordion-button collapsed fw-semibold fs-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                    Why conduct a JHA?  
+                </button>
+                </h2>
+                <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionFAQ">
+                    <div class="accordion-body fs-6">
+                        A JHA can prevent work-related injuries or illnesses by eliminating or controlling identified hazards. It is a means to ensure that workers have the training, equipment, and supplies to do their jobs safely.
+                    </div>
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingFive">
+                <button class="accordion-button collapsed fw-semibold fs-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                    What do I do when the JHA is completed?
+                </button>
+                </h2>
+                <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionFAQ">
+                    <div class="accordion-body fs-6">
+                        <p>1. Ensure equipment and supplies are acquired to perform the job/activity according to the JHA.</p>
+                        <p>2. Train workers in the procedures, controls, and PPE according to the JHA.</p>
+                        <p>3. Review the JHA periodically and update it when the job/activity or equipment changes. Revise JHA as needed and retrain employees on the revised JHA.</p>
+                        <p>4. Consult your administrative unit to review JHAs as needed.</p> 
+                    </div>  
+                </div>
+            </div>
+
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingSix">
+                <button class="accordion-button collapsed fw-semibold fs-5" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                    Other Information
+                </button>
+                </h2>
+                <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionFAQ">
+                    <div class="accordion-body fs-6">
+                        <p>The information in this FAQ section was obtained from the University of Washington's Environmental Health & Safety administrative unit (EH&S). More information regarding JHA's are available at the link below:</p>
+                        <p><a class="link-opacity-100" href="https://www.ehs.washington.edu/workplace/job-hazard-analysis">Job Hazard Analysis | EHS</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
